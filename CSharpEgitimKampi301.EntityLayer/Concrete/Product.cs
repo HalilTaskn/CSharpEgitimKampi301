0b14665af8f01fec463b355d3fa6d2b98f637344
haliltaşkın <haliltaskn1453@gmail.com>
2024-11-11 16:17:39 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Product
+    {
+        [Key]
+        public int ProduckId { get; set; }
+
+        public string ProduckName { get; set; }
+
+        public int ProductStock { get; set; }
+
+        public decimal ProductPrice{ get; set; }
+
+        public string ProduckDescription { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
+    
